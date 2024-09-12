8500f930ecb60fa6475024991a13788e7b6f0dcd
Saeful Rizal <saefulrizal@upi.edu>
2024-09-13 06:12:47 +0700

    final commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,12 +10,46 @@ const io = socketIo(server);
 // Melayani file statis dari folder public
 app.use(express.static('public'));
 
+// Koneksi ke MySQL menggunakan Sequelize
+const sequelize = new Sequelize('chat_app', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+// Model untuk pesan
+const Message = sequelize.define('Message', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    message: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    timestamp: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+    },
+});
+
+// Sinkronisasi model dengan database
+sequelize.sync();
+
 io.on('connection', (socket) => {
     console.log('A user connected');
 
+    // Mengirim riwayat pesan saat pengguna terhubung
+    Message.findAll({ order: [['timestamp', 'ASC']] }).then(messages => {
+        socket.emit('load messages', messages);
+    });
+
     // Menerima pesan dari client
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg); // Mengirimkan pesan ke semua client
+    socket.on('chat message', async (data) => {
+        const newMessage = await Message.create({
+            name: data.name,
+            message: data.message,
+        });
+        io.emit('chat message', newMessage); // Kirim pesan ke semua klien
     });
 
     // Notifikasi ketika user disconnect
@@ -25,6 +59,6 @@ io.on('connection', (socket) => {
 });
 
 // Menjalankan server
-server.listen(3000, () => {
-    console.log('Server listening on http://localhost:3000');
+server.listen(3000, '0.0.0.0', () => {
+    console.log('Server listening on http://0.0.0.0:3000');
 });
